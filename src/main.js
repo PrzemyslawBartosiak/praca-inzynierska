620522b3dc8660ae5d88e1dcceb760e8236ef0d2
Przemyslaw Bartosiak <przemek9614@gmail.com>
2021-01-06 14:31:01 +0100

    Template

--- src/main.js
+++ src/main.js
@@ -2,11 +2,32 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import vuetify from './plugins/vuetify';
+import firebase from 'firebase'
 
 Vue.config.productionTip = false;
+
+
+
+  
+var firebaseConfig = {
+  apiKey: "AIzaSyCjUG06ndYt8BEDoOmsbL8NCUJkXFV2B-c",
+  authDomain: "praca-inzynierska-45ab0.firebaseapp.com",
+  projectId: "praca-inzynierska-45ab0",
+  storageBucket: "praca-inzynierska-45ab0.appspot.com",
+  messagingSenderId: "192376754363",
+  appId: "1:192376754363:web:250e1f079ca6ae5bcdd99e",
+  measurementId: "G-T955M81R6S"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export const db = firebase.firestore();
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
